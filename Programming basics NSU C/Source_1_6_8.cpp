bac4bf6fa857a8a6a77223db82b7a5c7b3b28fd3
Терентий <ya.terenti.ak@yandex.ru>
2020-02-19 20:34:14 +0700

    b_sort

--- Programming basics NSU C/Source_1_6_8.cpp
+++ Programming basics NSU C/Source_1_6_8.cpp
@@ -1,104 +1,111 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//
-//#include "stdio.h"
-//#include "math.h"
-//
-//
-//
-////Р РµР°Р»РёР·Р°С†РёСЏ СЃРѕСЂС‚РёСЂРѕРІРєРё РїСѓР·С‹СЂСЊРєРѕРј. РџРѕ СѓРјРѕР»С‡Р°РЅРёСЋ, С„СѓРЅРєС†РёСЏ СЃРѕСЂС‚РёСЂСѓРµС‚ РїРѕ РІРѕР·СЂР°СЃС‚Р°РЅРёСЋ
-//void bubble_sort(void * begin, void * end, size_t elemsize);
-//
-////РћР‘Р РђРўРРўР• Р’РќРРњРђРќРР•, С„СѓРЅРєСѓС†РёСЏ СЃСЂР°РІРЅРµРЅРёСЏ РІРѕР·РІСЂР°С‰Р°РµС‚ true, РєРѕРіРґР° СЌР»РµРјРµРЅС‚С‹ СЂР°СЃРїРѕР»РѕР¶РµРЅС‹ РІ РџР РђР’РР›Р¬РќРћРњ РїРѕСЂСЏРґРєРµ.  
-//void bubble_sort(void * begin, void * end, size_t elemsize, bool(*in_right_order)(void *e1, void *e2));
-//
-//
-////cРѕСЂС‚РёСЂРѕРІРєР° РїРѕ РІРѕР·СЂР°СЃС‚Р°РЅРёСЋ
-//bool increasing(void *e1, void *e2) {
-//	return *(int*)e1 <= * (int *)e2;
-//}
-//
-////СЃРѕСЂС‚РёСЂРѕРІРєР° РїРѕ СѓР±С‹РІР°РЅРёСЋ
-//bool decreasing(void *e1, void *e2) {
-//	return *(int*)e1 >= * (int *)e2;
-//}
-//
-////РїРѕ СѓРІРµР»РёС‡РµРЅРёСЋ РјРѕРґСѓР»СЏ
-//bool abs_increasing(void *e1, void *e2) {
-//	return abs(*(int*)e1) >= abs(*(int *)e2);
-//}
-//
-//int main() {
-//	int arr[3] = { -2,  1, -4  };
-//
-//	bubble_sort(arr, arr +3, sizeof(int));
-//	return 0;
-//}
-//
-//
-//
-//
-//
-//
-//void bubble_sort(void * begin, void * end, size_t elemsize, bool(*in_right_order)(void *e1, void *e2))
-//{
-//	typedef signed char byte;
-//
-//	void swap(void*e1, void*e2, size_t size);
-//	byte *last = (byte*)end - elemsize;
-//	for (bool is_sorted = false; !is_sorted && begin != last; last-=elemsize) {
-//		is_sorted = true;
-//		for (byte*i = (byte*)begin; i != last; i+=elemsize) {
-//			if (!in_right_order(i, i + elemsize)) {
-//				swap(i, i + elemsize, elemsize);
-//				is_sorted = false;
-//			}
-//		}
-//	}
-//}
-//
-//
-//
-//void bubble_sort(void * begin, void * end, size_t elemsize)
-//{
-//	typedef signed char byte;
-//
-//	bool native_compare(void * e1, void * e2, size_t elemsize);
-//	void swap(void*e1, void*e2, size_t size);
-//	byte *last = (byte*)end - elemsize;
-//	for (bool is_sorted = false; !is_sorted && begin != last; last -= elemsize) {
-//		for (byte*i = (byte*)begin; i != last; i += elemsize) {
-//			if (!native_compare(i, i + elemsize, elemsize)) {
-//				swap(i, i + elemsize, elemsize);
-//				is_sorted = false;
-//			}
-//		}
-//	}
-//}
-//
-//
-//void swap(void*e1, void*e2, size_t size) {
-//
-//	typedef signed char byte;
-//
-//	for (byte*e2_i = (byte*)e2, *e1_i = (byte*)e1; 
-//		e2_i != (byte*)e2 + size; e1_i++, e2_i++) {
-//		//С„РёРЅС‚С‹ СѓС€Р°РјРё. РњРѕР¶РЅРѕ РЅРµРїРѕСЃСЂРµРґСЃС‚РІРµРЅРЅРѕ РїСЂРѕРІРµСЂРёС‚СЊ, С‡С‚Рѕ (a XOR b) XOR b = a Рё (a XOR b) XOR a = b
-//		*e1_i = *e1_i ^ *e2_i;
-//		*e2_i = *e1_i ^ *e2_i;
-//		*e1_i = *e1_i ^ *e2_i;
-//	}
-//
-//}
-//
-//
-//bool native_compare(void * e1, void * e2, size_t elemsize)
-//{
-//	typedef signed char byte;
-//	for (byte*i1 = (byte*)e1, *i2 = (byte*)e2, *end = (byte*)e1 + elemsize;
-//		e1 != end; i1++, i2++) {
-//		if (!(*i1 == *i2)) return (*i1 < *i2);
-//	}
-//	return true;
-//}
-//
-//
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "stdio.h"
+#include "math.h"
+
+
+
+//Реализация сортировки пузырьком. По умолчанию, функция сортирует по возрастанию
+void bubble_sort(void * begin, void * end, size_t elemsize);
+
+//ОБРАТИТЕ ВНИМАНИЕ, функуция сравнения возвращает true, когда элементы расположены в ПРАВИЛЬНОМ порядке.  
+void bubble_sort(void * begin, void * end, size_t elemsize, bool(*in_right_order)(void *e1, void *e2));
+
+
+//cортировка по возрастанию
+bool increasing(void *e1, void *e2) {
+	return *(int*)e1 <= * (int *)e2;
+}
+
+//сортировка по убыванию
+bool decreasing(void *e1, void *e2) {
+	return *(int*)e1 >= * (int *)e2;
+}
+
+//по увеличению модуля
+bool abs_increasing(void *e1, void *e2) {
+	return abs(*(int*)e1) >= abs(*(int *)e2);
+}
+
+int main() {
+	int arr[3] = { -2,  1, -4  };
+	//qsort???
+	bubble_sort(arr, arr +3, sizeof(int));
+	return 0;
+}
+
+
+
+
+
+
+void bubble_sort(void * begin, void * end, size_t elemsize, bool(*in_right_order)(void *e1, void *e2))
+{
+	typedef signed char byte;
+
+	void swap(void*e1, void*e2, size_t size);
+	byte *last = (byte*)end - elemsize;
+	for (bool is_sorted = false; !is_sorted && begin != last; last-=elemsize) {
+		is_sorted = true;
+		for (byte*i = (byte*)begin; i != last; i+=elemsize) {
+			if (!in_right_order(i, i + elemsize)) {
+				swap(i, i + elemsize, elemsize);
+				is_sorted = false;
+			}
+		}
+	}
+}
+
+
+
+void bubble_sort(void * begin, void * end, size_t elemsize)
+{
+	typedef signed char byte;
+
+	bool native_compare(void * e1, void * e2, size_t elemsize);
+	void swap(void*e1, void*e2, size_t size);
+	byte *last = (byte*)end - elemsize;
+	for (bool is_sorted = false; !is_sorted && begin != last; last -= elemsize) {
+		for (byte*i = (byte*)begin; i != last; i += elemsize) {
+			if (!native_compare(i, i + elemsize, elemsize)) {
+				swap(i, i + elemsize, elemsize);
+				is_sorted = false;
+			}
+		}
+	}
+}
+
+
+void swap(void*e1, void*e2, size_t size) {
+
+	typedef signed char byte;
+
+	for (byte*e2_i = (byte*)e2, *e1_i = (byte*)e1; 
+		e2_i != (byte*)e2 + size; e1_i++, e2_i++) {
+
+		byte add = *e1_i;
+		*e1_i = *e2_i;
+		*e2_i = add;
+
+		/*
+		//финты ушами. Можно непосредственно проверить, что (a XOR b) XOR b = a и (a XOR b) XOR a = b
+		*e1_i = *e1_i ^ *e2_i;
+		*e2_i = *e1_i ^ *e2_i;
+		*e1_i = *e1_i ^ *e2_i;
+		*/
+	}
+
+}
+
+
+bool native_compare(void * e1, void * e2, size_t elemsize)
+{
+	typedef signed char byte;
+	for (byte*i1 = (byte*)e1, *i2 = (byte*)e2, *end = (byte*)e1 + elemsize;
+		e1 != end; i1++, i2++) {
+		if (!(*i1 == *i2)) return (*i1 < *i2);
+	}
+	return true;
+}
+
+
