f3b0536a387acfb589e2f68a663f4041bda8b8e5
Терентий <ya.terenti.ak@yandex.ru>
2020-02-26 18:41:12 +0700

    Matrix Base

--- Programming basics NSU C/Source_2_7_9.cpp
+++ Programming basics NSU C/Source_2_7_9.cpp
@@ -7,29 +7,20 @@
 using namespace std;
 
 
+
 template< class Type>
 class Matrix {
-private:
-	size_t n = 0;
-    size_t m = 0;
-	Type **arr = NULL;
-	size_t *relations = NULL;
 
-	bool Empty() const {
-		return (n == 0 && m == 0 && arr == 0 && relations == 0);
-	}
-	Matrix(Type**arr, size_t*relations, size_t n=0,size_t m = 0 );//без учёта reletions
-	
-	class IteratorBase;
 public:
 
-	Matrix<Type> & operator=(const Matrix<Type> & other);
+	
 	
 	Matrix();
-	Matrix(size_t n, size_t m);
 	~Matrix();
+	Matrix(size_t n, size_t m);
 	Matrix(const Matrix<Type> & other);
-	Matrix(const Type & num, size_t n=0, size_t m=0);                // num*E
+	Matrix(const Type & num, size_t n, size_t m);                // num*E
+	Matrix<Type> & operator=(const Matrix<Type> & other);
 
 	size_t N() const;
 	size_t M() const;
@@ -40,8 +31,10 @@ public:
 	template<class Type>
 	friend std::istream & operator>>(std::istream&  in, Matrix<Type> & matrix);
 
-	explicit Matrix(Type**data, size_t n=0, size_t m = 0) :Matrix(data, new size_t(1), n, m) {}
+	explicit Matrix(Type**data, size_t n = 0, size_t m = 0);
 
+	template<class Type>
+	friend Matrix<Type> size_cast(Matrix<Type> & other, size_t n, size_t m);
 
 	//template<size_t k,size_t l> 
 	//explicit operator Matrix<Type>();
@@ -59,23 +52,41 @@ public:
 	Matrix<Type> operator+(const Matrix<Type> & other) const;
 	Matrix<Type> operator-(const Matrix<Type> & other) const;
 
+	Matrix<Type> operator+(const Type & num) const;
+	
+	template<class Type>
+	friend Matrix<Type> operator+(const Type & num, const Matrix<Type> & matrix);
+
 	Matrix<Type> operator*(const Type& num)const;
 	friend Matrix<Type> operator*(const Type & num, const Matrix<Type> & matrix);
-
-
+	
 	Matrix<Type> operator*(const Matrix<Type> & other) const;
 
+private:
+
+#define NO_BIND NULL
+
+	size_t n = 0;
+	size_t m = 0;
+	Type **arr = NULL;
+	size_t *relations = NO_BIND;
+
+	bool Empty() const;
+
+
+	Matrix(Type**arr, size_t*relations, size_t n = 0, size_t m = 0);//без учёта reletions
+
+	//class IteratorBase;
+	void initMatrix();
+	void deleteMatrix();
 	
 };
 
 
 int main() {
-	size_t size = 2;
-	Matrix<int> m, n;
-	std::cin >> n >> m;
-	std::cout << std::endl << n * m << std::endl << std::endl << m * n << std::endl;
-	Matrix<int> k = m, l = m;
-	//cout << l << std::endl;
+	Matrix<double> a(3,3), b(3,3);
+	cin >> a >> b;
+	cout << endl << a * b << endl;
 	return 0;
 }
 
@@ -86,34 +97,51 @@ int main() {
 
 
 template<class Type>
-Matrix<Type>::Matrix(Type ** arr, size_t * relations, size_t n, size_t m): n(n), m(m)  arr(arr), relations(relations) {}
+bool Matrix<Type>::Empty() const {
+	return (n == 0 && m == 0 && arr == 0 && relations == 0);
+}
+
+template<class Type>
+Matrix<Type>::Matrix(Type ** arr, size_t * relations, size_t n, size_t m): n(n), m(m),  arr(arr), relations(relations) {}
+
+template<class Type>
+void Matrix<Type>::initMatrix()
+{
+	
+	arr = new  Type*[m];
+	for (Type **i = arr, **end = arr + m; i != end; i++) {
+		*i = new Type[n];
+	}
+}
+
+template<class Type>
+void Matrix<Type>::deleteMatrix()
+{
+	for (Type ** i = arr, **end = arr + m; i != end; i++) {
+		delete[] * i;
+	}
+	delete[] arr;
+	delete relations;
+}
 
 template<class Type>
 Matrix<Type> & Matrix<Type>::operator=(const Matrix<Type>& other)
 {
-	if (n == 0 && m == 0 && arr == 0 relations == 0) {
-		//if(other.n == 0)
-		n = other.n;
-		m = other.m;
-		arr = other.arr;
-		relations = other.relations;
-		(*relations)++;
-		return *this;
+	if (Empty()) {
+		if (!other.Empty()) {
+			n = other.n; m = other.m; arr = other.arr; relations = other.relations;
+			if (other.relations != NO_BIND) (*relations)++;
+			return *this;
+		}
+		else return *this;
 	}
 	if ((n != other.n) || (m != other.m)) throw invalid_argument("Different matrix size");
-	if (*relations != 0)
-	{
-		(*relations)--;
-	}
-	else {
-		for (Type ** i = arr, **end = arr + m; i != end; i++) {
-			delete[] * i;
-		}
-		delete[] arr;
-		delete relations;
-	}
+	this->~Matrix();
 	
-	relations = other.relations; (*relations)++; arr = other.arr;
+	if (other.relations != NO_BIND) {
+		relations = other.relations; (*relations)++;
+	}
+	arr = other.arr;
 	return *this;
 }
 
@@ -123,17 +151,13 @@ Matrix<Type>::Matrix() {}
 template<class Type>
 Matrix<Type>::Matrix(size_t n, size_t m) :n(n), m(m), relations(new size_t(0))
 {
-	arr = new  Type* [m];
-
-	for (Type **i = arr, **end = arr + m; i != end; i++) {
-		*i = new Type[n];
-	}
+	initMatrix();
 }
 
 template<class Type>
 Matrix<Type>::Matrix(const Matrix<Type>& other) : Matrix(other.arr, other.relations, other.n, other.m)
 {
-	(*relations)++;
+	if (relations != NO_BIND) (*relations)++;
 }
 
 template<class Type>
@@ -147,16 +171,14 @@ Matrix<Type>::Matrix(const Type & num, size_t n, size_t m):Matrix(n, m)
 template<class Type>
 Matrix<Type>::~Matrix()
 {
-	if (*relations != 0) 
+	if (Empty() || relations == NO_BIND) return;
+
+	if (*relations != 0)
 	{
 		(*relations)--;
 	}
 	else {
-		for (Type ** i = arr, **end = arr + m; i != end; i++) {
-			delete[] * i;
-		}
-		delete[] arr;
-		delete relations;
+		deleteMatrix();
 	}
 }
 
@@ -173,6 +195,9 @@ size_t Matrix<Type>::M() const
 {
 	return m;
 }
+
+template<class Type>
+Matrix<Type>::Matrix(Type ** data, size_t n, size_t m) :Matrix(data, NO_BIND, n, m) {}
 
 //template<class Type, size_t n, size_t m>
 //Type ** Matrix<Type, n, m>::__data__()
@@ -233,6 +258,12 @@ Matrix<Type> Matrix<Type>::operator-(const Matrix<Type>& other) const
 }
 
 template<class Type>
+Matrix<Type> Matrix<Type>::operator+(const Type & num) const
+{
+	return this->operator+(Matrix<Type>(num, n, m));
+}
+
+template<class Type>
 Matrix<Type> Matrix<Type>::operator*(const Type & num) const
 {
 	Matrix<Type> ans(n, m);
@@ -259,13 +290,13 @@ template<class Type>
 std::ostream & operator<<(std::ostream & out, const Matrix<Type> & matrix)
 {
 	for (
-		Type** i_this = matrix.arr, ** i_end = matrix.arr + m, **i_last = matrix.arr+m-1;
+		Type** i_this = matrix.arr, ** i_end = matrix.arr + matrix.m, **i_last = matrix.arr+matrix.m-1;
 		i_this != i_end;
 		i_this++
 		) {
 
 		for (
-			Type* j_this = *i_this, *j_end = *i_this + n;
+			Type* j_this = *i_this, *j_end = *i_this + matrix.n;
 			j_this != j_end;
 			j_this++
 			) {
@@ -281,13 +312,13 @@ template<class Type>
 std::istream & operator>>(std::istream & in, Matrix<Type>& matrix)
 {
 	for (
-		Type** i_this = matrix.arr, ** i_end = matrix.arr + m;
+		Type** i_this = matrix.arr, ** i_end = matrix.arr + matrix.m;
 		i_this != i_end;
 		i_this++
 		) {
 
 		for (
-			Type* j_this = *i_this, *j_end = *i_this + n;
+			Type* j_this = *i_this, *j_end = *i_this + matrix.n;
 			j_this != j_end;
 			j_this++
 			) {
@@ -298,18 +329,31 @@ std::istream & operator>>(std::istream & in, Matrix<Type>& matrix)
 	return in;
 }
 
+
+template<class Type>
+Matrix<Type> size_cast(Matrix<Type>& other, size_t n, size_t m)
+{
+	return Matrix<Type>(other.arr, NO_BIND, n, m);
+}
+
+template<class Type>
+Matrix<Type> operator+(const Type & num, const Matrix<Type>& matrix)
+{
+	return Matrix<Type>(num, matrix.n, matrix.m).operator+(matrix);
+}
+
 template<class Type>
 Matrix<Type> operator*(const Type & num, const Matrix<Type>& matrix)
 {
-	Matrix<Type> ans(n,m);
+	Matrix<Type> ans(matrix.n,matrix.m);
 	for (
-		Type** i = matrix.arr,** i_ans = ans.arr,** i_end = matrix.arr + m;
+		Type** i = matrix.arr,** i_ans = ans.arr,** i_end = matrix.arr + matrix.m;
 		i != i_end;
 		i++, i_ans++
 		) {
 
 		for (
-			Type* j = *i,* j_ans = *i_ans,* j_end = *i + n;
+			Type* j = *i,* j_ans = *i_ans,* j_end = *i + matrix.n;
 			j != j_end;
 			j++, j_ans++
 			) {
@@ -334,6 +378,7 @@ template<class Type>
 Matrix<Type> Matrix<Type>::operator*(const Matrix<Type>& other) const
 {
 	if (n != other.m) throw invalid_argument("Different matrix size");
+
 	Matrix<Type> mult(other.n, m);
 	unsigned i = 0, j = 0;
 	for (Type**i_mult = mult.arr,** i_end = mult.arr + m; i_mult != i_end; i_mult++, i++) {
